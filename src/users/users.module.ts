e3fc0843c0fd742ee6d38977cf2fc261fae35be8
Alexey Bagishev <alexey.bagishev.dev@gmail.com>
2024-05-16 12:36:36 +0300

    issue-88/feat: added aws s3 storage

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import {MongooseModule} from "@nestjs/mongoose"
 import {AuthModule} from "src/auth/auth.module"
 import {UsersService} from "./users.service"
 import {User, UsersSchema} from "./users.schema"
-import {CDNModule} from "src/cdn/cdn.module"
+import {StorageModule} from "src/storage/storage.module"
 import {EmailService} from "src/email/email.service"
 
 @Module({
@@ -13,7 +13,7 @@ import {EmailService} from "src/email/email.service"
       schema: UsersSchema
     }]),
     forwardRef(() => AuthModule),
-    CDNModule
+    StorageModule
   ],
   providers: [UsersService, EmailService],
   exports: [UsersService, EmailService]
